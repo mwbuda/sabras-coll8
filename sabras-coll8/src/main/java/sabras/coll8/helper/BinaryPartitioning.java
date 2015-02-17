f3879c5cfc7ce9d4467554b2ddc889c7166b7630
mwbuda <mbuda@sabrassoft.com>
2015-02-16 22:25:22 -0500

    + binary partitioning for partitions from byte[]'s

---
+++ sabras-coll8/src/main/java/sabras/coll8/helper/BinaryPartitioning.java
@@ -0,0 +1,59 @@
+package sabras.coll8.helper;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+
+public interface BinaryPartitioning<X>
+extends Function<X,List<byte[]>>{
+
+	public class IntoMax<X>
+	implements BinaryPartitioning<X> {
+		
+		public static IntoMax<Object> simple(Integer max) {
+			return new IntoMax<>(max, x -> x.toString().getBytes()) ;
+		}
+		
+		public IntoMax<byte[]> fromByteArray(Integer max) {
+			return new IntoMax<>(max, x -> x) ;
+		}
+		
+		private final Integer maxPartitonSize ;
+		private final Function<X,byte[]> transform ;
+		
+		public IntoMax(Integer max, Function<X,byte[]> t) {
+			this.maxPartitonSize = max ;
+			this.transform = t ;
+			
+		}
+		
+		protected byte[] createPartition() {
+			return new byte[this.maxPartitonSize] ;
+		}
+		
+		@Override
+		public final List<byte[]> apply(X x) {
+			byte[] rawConvert = this.transform.apply(x) ;
+			int totalByteCount = rawConvert.length ;
+			int partitionCount = totalByteCount / this.maxPartitonSize ;
+			int derivedByteCount = partitionCount * this.maxPartitonSize ;
+			if (totalByteCount > derivedByteCount) partitionCount += 1 ;
+			
+			List<byte[]> results = new LinkedList<>() ;
+			int s = 0 ;
+			int l = this.maxPartitonSize ;
+			int copied = 0 ;
+			while (results.size() < partitionCount) { 
+				byte[] currentPartition = this.createPartition() ;
+				System.arraycopy(rawConvert, s, currentPartition, 0, l);
+				results.add(currentPartition) ;
+				s += this.maxPartitonSize ;
+				copied = s ;
+				if ((copied + l) > totalByteCount) l = (totalByteCount - copied) ;
+			}
+			
+			return results;
+		}
+	}
+	
+}
