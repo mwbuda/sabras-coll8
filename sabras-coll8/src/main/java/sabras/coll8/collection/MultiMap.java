d4b958e0142827e637b7b8844b41c0b1fc0d8473
mwbuda <mbuda@sabrassoft.com>
2015-07-26 16:52:20 +0000

    + partial removes to multimaps

--- sabras-coll8/src/main/java/sabras/coll8/collection/MultiMap.java
+++ sabras-coll8/src/main/java/sabras/coll8/collection/MultiMap.java
@@ -1,12 +1,13 @@
 package sabras.coll8.collection;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import sabras.coll8.helper.CollectConvert;
 
-public interface MultiMap<K,V,C extends Iterable<V>> extends Map<K, C> {
+public interface MultiMap<K,V,C extends Collection<V>> extends Map<K, C> {
 
 	@SuppressWarnings("unchecked")
 	default public C appendOne(K k, V v) {
@@ -28,12 +29,33 @@ public interface MultiMap<K,V,C extends Iterable<V>> extends Map<K, C> {
 	default public C put(K k, V... vs) {
 		return this.put(k, CollectConvert.newIterable(vs)) ;
 	}
+	default C put(K k, C vs) {
+		return this.put(k, CollectConvert.newIterable(vs)) ;
+	}
 	public C put(K k, Iterable<V> vs) ;
 	
 	default public Iterable<V> allValues() {
 		List<V> allvs = CollectConvert.newList() ;
 		for (C vs : this.values()) if (vs != null) allvs.addAll(CollectConvert.newCollection(vs)) ;
 		return allvs ;
+	}
+	
+	default public C removeAll(K k) {
+		return this.remove(k) ;
+	}
+	default public C removeOne(K k, V v) {
+		return this.removeSome(k, CollectConvert.newIterable(v)) ;
+	}
+	@SuppressWarnings("unchecked")
+	default public C removeSome(K k, V... vs) {
+		return this.removeSome(k, CollectConvert.newIterable(vs)) ;
+	}
+	default public C removeSome(K k, Iterable<V> vs) {
+		Set<V> uvs = CollectConvert.newSet(vs) ;
+		C xvs = this.get(k) ;
+		List<V> nvs = CollectConvert.newList(xvs) ;
+		boolean didChange = nvs.removeAll(uvs) ;
+		return didChange ? this.put(k, nvs) : xvs ;
 	}
 	
 	default public Set<V> allUniqueValues() {
